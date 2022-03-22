462807b52a630bbb18bb418903a8f87645e35c69
imankns70 <45065474+imankns70@users.noreply.github.com>
2022-03-22 18:00:09 +0430

    WebSocketServer is just done

---
+++ WebSocketServer/WebSocketServer/Middleware/WebSocketServerConnectionManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace WebSocketServer.Middleware
+{
+    public class WebSocketServerConnectionManager
+    {
+        private ConcurrentDictionary<string, WebSocket> _socket = new ConcurrentDictionary<string, WebSocket>();
+
+        public ConcurrentDictionary<string, WebSocket> GetAllSockets()
+        {
+            return _socket;
+        }
+
+        public string AddSocket(WebSocket socket)
+        {
+            string connID = Guid.NewGuid().ToString();
+            _socket.TryAdd(connID, socket);
+            Console.WriteLine("Connection Added:" + connID);
+            return connID;
+        }
+    }
+}
---
+++ WebSocketServer/WebSocketServer/Startup.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using WebSocketServer.Middleware;
+
+namespace WebSocketServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddWebSocketManager();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+
+            //app.UseRouting();
+            
+
+            app.UseWebSockets();
+            app.UseWebSocketServer();
+
+
+
+            app.Run(async context =>
+            {
+                Console.WriteLine("Hello from the 3rd request delegate.");
+
+                await context.Response.WriteAsync("Hello from the 3rd request delegate.");
+            });
+
+        }
+     
+    }
+
+
+}
