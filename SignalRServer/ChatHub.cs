be460ebf298d4ce4c880730e1896235c62cb17b8
imankns70 <45065474+imankns70@users.noreply.github.com>
2022-03-23 19:41:51 +0430

    signalR was done

---
+++ SignalRServer/ChatHub.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+
+namespace SignalRServer
+{
+    public class ChatHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine("--> Connection Established :" + Context.ConnectionId);
+
+            Clients.Client(Context.ConnectionId).SendAsync("ReceiveConnID :", Context.ConnectionId);
+
+            return base.OnConnectedAsync();
+
+        }
+
+        public async Task SendMessageAsync(string message)
+        {
+            var routeOb = JsonConvert.DeserializeObject<dynamic>(message);
+
+            string toCLient = routeOb.To;
+
+            Console.WriteLine("--> Message Received on:" + Context.ConnectionId);
+
+            if (string.IsNullOrEmpty(toCLient))
+            {
+                await Clients.All.SendAsync("ReceiveMessage", message);
+            }
+            else
+            {
+                await Clients.Client(toCLient).SendAsync("ReceiveMessage", message);
+            }
+
+
+        }
+    }
+}
